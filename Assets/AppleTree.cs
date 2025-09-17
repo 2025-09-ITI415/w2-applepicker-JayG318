8b17bb5dc50854372f26f35b564c638ba5f05907
J Goldenberg <jag629@scarletmail.rutgers.edu>
2025-09-16 21:18:36 -0400

    Added Tree Movement & Direction Changes

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -26,6 +26,24 @@ public class AppleTree : MonoBehaviour
     void Update()
     {
         // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
         // Changing Direction
+        if (pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed); // Move right
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed); //Move left
+        // } else if (Random.value < changeDirChance) {
+        //     speed *= -1; // Negative speed changes direction
+        }
+    }
+
+    void FixedUpdate() {
+        // Random direction changes are now time-based due to FixedUpdate()
+        if (Random.value < changeDirChance) {
+            speed *= -1; // Change direction
+        }
     }
 }
